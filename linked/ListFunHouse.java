3c09499aa2331b9a450ff7f1080fa15120f55b02
Adii70 <fire.slayer7gold@gmail.com>
2024-12-17 22:32:47 -0600

    edsada

--- linked/ListFunHouse.java
+++ linked/ListFunHouse.java
@@ -59,9 +59,7 @@ public class ListFunHouse
 	{
 
 		while (list.getNext() != null){
-			if (list.getNext().getNext()!=null){
-				list = list.getNext().getNext();
-			}
+				list = list.getNext();
 		}
 	}
 
